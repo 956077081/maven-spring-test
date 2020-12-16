6141b34f80e5978fab2148f754ddc2d9b4a6e628
pht <9560770781@qq.com>
2020-12-16 20:21:15 +0800

    itext pdf  签名

--- src/main/java/com/pht/pdf/PdfOperator.java
+++ src/main/java/com/pht/pdf/PdfOperator.java
@@ -1,10 +1,17 @@
 package com.pht.pdf;
 
+import com.alibaba.fastjson.JSONObject;
 import com.itextpdf.forms.PdfAcroForm;
 import com.itextpdf.forms.fields.PdfFormField;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfArray;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.annot.PdfWidgetAnnotation;
+import org.apache.poi.hssf.record.formula.functions.T;
+import org.codehaus.groovy.util.StringUtil;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,7 +27,33 @@ public class PdfOperator {
         formFields.forEach((name,formField)->{
             List<PdfWidgetAnnotation> widgets = formField.getWidgets();
             for (PdfWidgetAnnotation widget : widgets) {
+                PdfPage page = widget.getPage();
+                print(pdfDocument.getPageNumber(page));
+                print(page.getPageSize());
+                print(page.getPageSize().getHeight());
+                print(page.getPageSize().getWidth());
+                Rectangle rectangle = widget.getRectangle().toRectangle();//获取签名域
+                print(rectangle);
+                print(rectangle.getX());//签名域X 坐标 中心角为左下角
+                print(rectangle.getY());//签名域Y 坐标
+                print(rectangle.getHeight());//高度
+                print(rectangle.getWidth());//宽度
+                print(rectangle.getBottom());//下高度
+                print(rectangle.getTop());//上高度
+                print(rectangle.getRight());//右侧距离
+                print(rectangle.getLeft());//左侧距离
             }
+            if(!StringUtils.isEmpty(formField.getValue())){
+                try {
+                    System.out.println("--------------------------------"+new String(formField.getValue().toString().getBytes("GB2312"), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+
         });
+    }
+    public static  void print(Object messafge){
+        System.out.println(messafge);
     }
 }
